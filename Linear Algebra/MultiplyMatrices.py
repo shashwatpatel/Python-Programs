32bb27a96512b40af38c098e9d4fb3f568859491
Shashwat Patel <35824714+shashwatpatel@users.noreply.github.com>
2019-06-14 14:49:33 -0700

    Add files via upload

---
+++ Linear Algebra/MultiplyMatrices.py
@@ -0,0 +1,14 @@
+import numpy as x
+
+#Define matrix (Could be changed)
+a = [[1,0], [0,1]]
+b = [[1,2], [3,4]]
+#Print the given matrix
+print("Given Matrix: ")
+print(a)
+print(b)
+#Print solution
+prod = x.dot(a, b)
+print('Multiplied Matrix: ')
+print(prod)
+
