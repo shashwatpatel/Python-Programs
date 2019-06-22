d937541fdbd1222eb7da4b0d07886ddd1176ec8a
Shashwat Patel <35824714+shashwatpatel@users.noreply.github.com>
2019-06-22 12:38:26 -0700

    Update Eigenvalues.py

--- Linear Algebra/Eigenvalues.py
+++ Linear Algebra/Eigenvalues.py
@@ -1,7 +1,10 @@
 import numpy as x
+#Define matrix/squared array (Can be changed)
 a = x.mat("3 -2;1 0")
+#Print the given matrix
 print('Givwn matrix: ')
 print("a\n", a)
+#Perform operations to get the eigenvalues and eigenvectors and print them
 p, q = x.linalg.eig(a)
 print('Eigenvalues of given matrix ', p)
 print('Eigenvectors of given matrix ', q)
